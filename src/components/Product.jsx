423b97f4a26ed08a592aae268777f0b44a461b4d
Subrat Kumar Pattanaik <subratpattanaik@Subrats-MacBook-Air.local>
2024-01-28 14:59:41 +0530

    modified product and productscroll components

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -9,22 +9,28 @@ const Product = ({
   minQuantity = 0,
 }) => {
   return (
-    <div className="flex justify-center w-full p-1 max-w-screen-lg">
-      <div className="m-1 p-1 w-3/12 h-full self-center">
-        <img src={imgsrc} alt="Image not found" />
+    <div className="flex self-center justify-center w-full p-1">
+      <div className="flex flex-col m-1 p-1 w-3/12 h-28 justify-center">
+        <img
+          className="max-h-28 self-center"
+          src={imgsrc}
+          alt="Image not found"
+        />
       </div>
       <div className="m-1 p-1 w-6/12">
         <div className="block m-1">{title}</div>
         <div className="block text-sm m-1">{variant}</div>
-        <div className="block text-sm m-1">
+        <div className="flex text-sm m-1">
           <button
+            className="rounded-none"
             onClick={() => setQuantity(quantity - 1)}
             disabled={quantity === minQuantity}
           >
             -
           </button>
-          <span className="m-2">{quantity}</span>
+          <div className="self-center m-1 min-w-6 text-center">{quantity}</div>
           <button
+            className="rounded-none"
             onClick={() => setQuantity(quantity + 1)}
             disabled={quantity === maxQuantity}
           >
--- src/components/ProductScroll.jsx
+++ src/components/ProductScroll.jsx
@@ -15,15 +15,31 @@ const initialState = [
     id: 2,
     title: "Product 2",
     variant: "Variant 2",
-    quantity: 1,
+    quantity: 1000,
     price: 2000,
-    imgsrc:
-      "https://images.unsplash.com/photo-1519141266120-200000000000?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    imgsrc: "https://images.meesho.com/images/products/8774222/a64fe_512.webp",
   },
   {
     id: 3,
     title: "Product 3",
     variant: "Variant 3",
+    quantity: 1,
+    price: 3000,
+    imgsrc:
+      "https://cdn.shopify.com/s/files/1/0070/7032/files/universal_20product_20code.png?v=1697911576",
+  },
+  {
+    id: 4,
+    title: "Product 4",
+    variant: "Variant 4",
+    quantity: 1,
+    price: 3000,
+    imgsrc: "https://m.media-amazon.com/images/I/81PtF30TLUL._AC_UY1100_.jpg",
+  },
+  {
+    id: 5,
+    title: "Product 5",
+    variant: "Variant 5",
     quantity: 1,
     price: 3000,
     imgsrc:
@@ -48,15 +64,25 @@ function ProductScroll() {
   return (
     <div className="justify-center items-center flex flex-col">
       {products.map((product) => (
-        <Product
+        <div
           key={product.id}
-          title={product.title}
-          variant={product.variant}
-          quantity={product.quantity}
-          price={product.price}
-          imgsrc={product.imgsrc}
-          setQuantity={(quantity) => updateQuantity(product.id, quantity)}
-        />
+          className="bg-zinc-800 shadow-md shadow-zinc-900 m-1 p-1 w-full flex flex-col self-center justify-center max-w-screen-lg"
+        >
+          <Product
+            title={product.title}
+            variant={product.variant}
+            quantity={product.quantity}
+            price={product.price}
+            imgsrc={product.imgsrc}
+            setQuantity={(quantity) => updateQuantity(product.id, quantity)}
+          />
+          <div className="m-1 p-1">
+            <button className="border-zinc-800 rounded-none">Remove</button>
+            <button className="border-zinc-800 rounded-none">
+              Buy this now
+            </button>
+          </div>
+        </div>
       ))}
     </div>
   );
